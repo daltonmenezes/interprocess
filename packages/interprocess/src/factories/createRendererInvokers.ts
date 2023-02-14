95b73a03086ad69bc1a9c56c96fd100867469152
Dalton Menezes <daltonmenezes@outlook.com>
2023-02-14 00:51:27 -0300

    fix: type issue on invokers with zero arguments - fixes #6

--- packages/interprocess/src/factories/createRendererInvokers.ts
+++ packages/interprocess/src/factories/createRendererInvokers.ts
@@ -16,7 +16,9 @@ export function createRendererInvokers<T extends IPCFactoryProps<T>>(props: T) {
     }
   }, {}) as {
     [Property in MainKeys]: (
-      arg: Parameters<Main[Property]>[1],
+      arg: Parameters<Main[Property]>[1] extends undefined
+        ? void
+        : Parameters<Main[Property]>[1],
       ...restOfArgs: any[]
     ) => ReturnType<Main[Property]>
   }
